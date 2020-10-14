6853c83ff051ccf1b3033ff09fc1779f92ee1f20
tobyl <tobyleftly@gmail.com>
2020-10-14 11:52:57 -0400

    Updates

---
+++ precache-manifest.0eca35dc2d65bfff9dda0cdf829aa1eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad420c0a36d2f5609bc0a9964399199a",
+    "url": "/qr/index.html"
+  },
+  {
+    "revision": "bdb1d93814e4a57005b5",
+    "url": "/qr/static/css/main.c0ad9464.chunk.css"
+  },
+  {
+    "revision": "99d538f06da866b8cd39",
+    "url": "/qr/static/js/2.89f6b05f.chunk.js"
+  },
+  {
+    "revision": "1268ca0b2cb565dff1685e55eba88fd9",
+    "url": "/qr/static/js/2.89f6b05f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb1d93814e4a57005b5",
+    "url": "/qr/static/js/main.f3faef56.chunk.js"
+  },
+  {
+    "revision": "a8da28899a0d9517468e",
+    "url": "/qr/static/js/runtime-main.fb748db2.js"
+  }
+]);
